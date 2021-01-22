1b38fea48472ebcb41d621f8140197381941a564
Trevor Guinn <tdguinn87@gmail.com>
2021-01-21 20:59:10 -0600

    Created paramaters for canvas and test ball

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// Function takes 2 numbers as arguments, and returns a random number in the range between the two
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
+
+// Adding a constructor to model the behavior of the balls
+  function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  Ball.prototype.draw = function() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+  }
